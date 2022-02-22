3e768af8298be867dfad6ebdb06162f0cf44487d
miqueiasbelfort <miqueiasbelfort8323@gmail.com>
2022-02-22 14:44:57 -0300

    first commit

--- front/src/models/ServicesLogin/login/Login.jsx
+++ front/src/models/ServicesLogin/login/Login.jsx
@@ -1,17 +1,31 @@
-import {BoxMaster} from "./styles"
-import LoginImg1 from '../../imgs/login1.svg'
+import {BoxMaster, MaxContainer, Container, DisplayFlexDiv, Button, GitButton} from './styles'
+import LoginImg from '../../imgs/login1.svg'
+import Logo from '../../imgs/Pluss+Class_Logo.svg'
+import {FaGoogle,FaGithub} from 'react-icons/fa'
 
 function Login(){
     return (
         <BoxMaster>
-            <div>
-                <img src={LoginImg1} alt="imgLogin" />
-            </div>
-            <div>
-                
-            </div>
-        </BoxMaster>      
+
+            <img src={LoginImg} alt="Imagem Login"/>
+            
+            <MaxContainer>
+
+                <img src={Logo} alt="Logo Pluss+Class" />
+
+                <Container>
+
+                    <h2>Entrar com...</h2>
+
+                    <DisplayFlexDiv>
+                        <Button><FaGoogle/>Google</Button>
+                        <Button><FaGithub/>GitHub</Button>
+                    </DisplayFlexDiv>
+
+                </Container>
+
+            </MaxContainer>
+        </BoxMaster>
     )
 }
-
 export default Login
--- front/src/models/ServicesLogin/login/styles.js
+++ front/src/models/ServicesLogin/login/styles.js
@@ -4,4 +4,60 @@ export const BoxMaster = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
+    align-items: center;
+    overflow-y: hidden;
+
+    img{
+        width: 60%;
+        background: cover;
+        filter: brightness(80%);
+    }
 `;
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    img{
+        width: 300px;
+    }
+`
+
+export const MaxContainer = styled.div`
+    width: 40%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    gap: 40px;
+`
+
+export const DisplayFlexDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 30px;
+    margin-top: 20px;
+`
+export const Button = styled.button`
+    width: 200px;
+    padding: 10px;
+    border-radius: 8px;
+    background: #FCA213;
+    border: none;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    font-weight: bold;
+    transition: all .2s ease;
+    cursor: pointer;
+`
+
+export const GitButton = styled.button`
+    background: #000;
+`
